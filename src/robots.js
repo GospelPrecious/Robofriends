e0a30476a94f15217ab98890874cf88c8be41df3
Gospel Precious <Gospelp66@gmail.com>
2024-08-29 14:17:43 -0700

    first commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Dinine Angel',
+    username: 'Atom',
+    email: 'atom@nistar.tv'
+  },
+  {
+    id: 2,
+    name: 'Olisa san',
+    username: 'Santa',
+    email: 'Olisa@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'David jack',
+    username: 'Small',
+    email: 'Small@jack.tv'
+  },
+  {
+    id: 4,
+    name: 'Steeze 001',
+    username: '001',
+    email: 'Steeze@001.tv'
+  },
+  {
+    id: 5,
+    name: 'Hurk cane',
+    username: 'Cane',
+    email: 'Huek@cane.tv'
+  },
+  {
+    id: 6,
+    name: 'Shoun paul',
+    username: 'Shoun',
+    email: 'Paul@shoun.tv'
+  },
+  {
+    id: 7,
+    name: 'Dan seant',
+    username: 'Seant',
+    email: 'Dan@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'sammy B',
+    username: 'Billions',
+    email: 'Sammmy@gmail.com'
+  }
+];
